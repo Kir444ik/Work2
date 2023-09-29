d04e8decb326c55dfab9fa97edda002eb3a61745
Kir444ik <144593324+Kir444ik@users.noreply.github.com>
2023-09-29 18:53:10 +0300

    end 15

---
+++ EX_15/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Enter day");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day < 8)
+{
+    if ( day >= 6  )
+    {
+        Console.WriteLine("yes");
+    } 
+    else
+    {
+        Console.WriteLine("No");
+    }
+}
